58e51001d23778be1c4e2c7e6003d5b874a0e6d8
shany <shanykelrich@gmail.com>
2026-09-10 17:28:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Delegates;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        delegate bool ClassFilter(Student student);
+
+        static bool IsExcellent(Student student)
+        {
+            return student.Name == "avi";
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Student student = new Student("avi", "יא7");
+
+            ClassFilter myFilter = IsExcellent;
+
+            Console.WriteLine(myFilter(student));
+            myFilter = delegate (Student student){
+                return student.StudentClass == "יב1";
+            };
+            myFilter = (Student student) =>
+            {
+                return student.StudentClass == "יב1";
+            };
+            myFilter = student => student.StudentClass == "יב1";
+            Func<Student,string > getName = student => student.Name.ToUpper();
+            Action<Student> printStudent = student => Console.WriteLine($"Name: {student.Name}, Class: {student.StudentClass}");
+            Predicate<Student> nameLongerThan4 = student => student.Name.Length > 4;
+            Action<Student> printName = student => Console.WriteLine(student.Name);
+            printName += student => Console.WriteLine(student.StudentClass);
+            List<Student> students = new List<Student>
+            {
+                new Student("avi", "יא7"),
+                new Student("david", "יב1"),
+                new Student("michael", "2יב"),
+                new Student("noa", "יא7"),
+                new Student("rachel", "יב1")
+            };  
+            int count=students.Count(student=>student.StudentClass=="יב2");
+            Student student1=students.Find(student => student.Name == "noa");
+            List<Student> students12 = students.Where(student => student.StudentClass == "יב3").ToList();
+
+
+
+
+
+        }
+    }
+}
+
+
---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string StudentClass { get; set; }
+
+        public Student(string name, string studentClass)
+        {
+            Name = name;
+            StudentClass = studentClass;
+        }
+    }
+}
